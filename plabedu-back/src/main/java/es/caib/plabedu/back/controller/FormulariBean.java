57235399854be938d6e38dcd4bb28aa84fb397b7
jrivas1913 <jose.miguel.rivas.22@gmail.com>
2021-05-26 11:00:36 +0200

    S'ha afegit javadoc. S'ha  creat un array d'Integer que s'omple en funció de la quantitat de places que hi hagi.

--- plabedu-back/src/main/java/es/caib/plabedu/back/controller/FormulariBean.java
+++ plabedu-back/src/main/java/es/caib/plabedu/back/controller/FormulariBean.java
@@ -1,6 +1,7 @@
 package es.caib.plabedu.back.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
@@ -15,11 +16,18 @@ import es.caib.plabedu.back.model.Plaza;
 
 @ManagedBean
 @RequestScoped
+/**
+ * Controla la lògica del formulari.xhtml.
+ * 
+ * @author e41621391q
+ *
+ */
 public class FormulariBean {
 
 	private Formulari formulari = new Formulari();
 	private static List<Formulari> lista = new ArrayList<>();
-	private static List<Plaza>listaPlazas = new ArrayList<>();
+	private static List<Plaza> listaPlazas = new ArrayList<>();
+	private static Integer[] numPlaza;
 
 	public Formulari getFormulari() {
 		return formulari;
@@ -36,8 +44,6 @@ public class FormulariBean {
 	public void setLista(List<Formulari> lista) {
 		FormulariBean.lista = lista;
 	}
-	
-	
 
 	public List<Plaza> getListaPlazas() {
 		return listaPlazas;
@@ -47,32 +53,52 @@ public class FormulariBean {
 		FormulariBean.listaPlazas = listaPlazas;
 	}
 
-	public void registrar() {
-		//FormulariBean.lista.add(this.formulari);
-		//FormulariBean.lista.add(new Formulari("Antonio","Ribas",611235412,"anribas@gmail.com"));
-		FormulariBean.listaPlazas.add(new Plaza("IES JUNIPER SERRA (Palma)",0));
-		FormulariBean.listaPlazas.add(new Plaza("IES MOSSEN ALCOVER (Manacor)",0));
-		FormulariBean.listaPlazas.add(new Plaza("IES SON FERRER (Calvià)",0));
-		FormulariBean.listaPlazas.add(new Plaza("IES SANTA MARGALIDA (Santa Margalida)",0));
-		FormulariBean.listaPlazas.add(new Plaza("IES CAPDEPERA (Capdepera)",0));
+	public Integer[] getNumPlaza() {
+		return numPlaza;
 	}
 
-	public String obtenerDatos() {
-		return FormulariBean.lista.get(0).toString();
+	public void setNumPlaza(Integer[] numPlaza) {
+		FormulariBean.numPlaza = numPlaza;
 	}
 
 	/**
+	 * Afegeix un objecte Formulari al dataTable.
+	 */
+	public void registrar() {
+		FormulariBean.lista.add(this.formulari);
+
+	}
+
+	/**
+	 * Omple la listaPlazas amb objectes Plaza per visualitzar-los al dataTable.
+	 * Inicilitza l'array numPlaza i l'omple amb el nombre de places que s'han afegit.
+	 */
+	public void afegirPlaza() {
+		FormulariBean.listaPlazas.add(new Plaza("IES JUNIPER SERRA (Palma)", 0));
+		FormulariBean.listaPlazas.add(new Plaza("IES MOSSEN ALCOVER (Manacor)", 0));
+		FormulariBean.listaPlazas.add(new Plaza("IES SON FERRER (Calvià)", 0));
+		FormulariBean.listaPlazas.add(new Plaza("IES SANTA MARGALIDA (Santa Margalida)", 0));
+		FormulariBean.listaPlazas.add(new Plaza("IES CAPDEPERA (Capdepera)", 0));
+		FormulariBean.listaPlazas.add(new Plaza("IES CALVIA (Calvia)", 0));
+		FormulariBean.listaPlazas.add(new Plaza("IES SANTA MARGALIDA (Santa Margalida)", 0));
+		FormulariBean.numPlaza = new Integer[FormulariBean.listaPlazas.size() + 1];
+		for (int i = 0; i < FormulariBean.listaPlazas.size() + 1; i++) {
+			FormulariBean.numPlaza[i] = i;
+		}
+	}
+
+	/**
+	 * Valida el inputText "email". Si el String compleix la condició amollarà un
+	 * error de validació.
 	 * 
-	 * @param context    Podemos tener acceso a todas las variables.
-	 * @param toValidate El componente que invoca este método
-	 * @param value      El valor que trae consigo el componente
+	 * @param context    Podem tenir accés a totes les variables.
+	 * @param toValidate El component que invoca aquest mètode.
+	 * @param value      El valor que dutrae consigo el componente
 	 */
 	public void validar(FacesContext context, UIComponent toValidate, Object value) {
 		context = FacesContext.getCurrentInstance();
-		// capturamos el valor del objeto
 		String email = (String) value;
-		String[] array = email.split("@");
-		if (array == null) {
+		if (!email.contains("@")) {
 			((UIInput) toValidate).setValid(false);
 			context.addMessage(toValidate.getClientId(context), new FacesMessage("Email no válido"));
 		}
